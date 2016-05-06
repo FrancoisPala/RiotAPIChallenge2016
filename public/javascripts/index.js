a0d3b54d3d37e406876e707c8722bcc5bf712560
DrissTM <driss.tahraouimaldague@gmail.com>
2016-05-06 23:59:20 +0200

    NA default server + div for each champion

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -9,8 +9,20 @@ function validate() {
     socket.emit("client info", summName + "," + region);
     return false;
 }
+
+function displayChampInfos(elem, index, array) {
+    $("#Container").append('<div class=" mix '+ 'champion-'+elem.name +' '+ 'points-'+elem.championPoints +'">' + elem.name + '\nPoints: ' + elem.championPoints + '</div>');
+}
+
 socket.on("info sent", function (data) {
+    
     console.log("received this: " + data);
+    console.log(data.length);
+    console.log(data[0]);
+    data.forEach(displayChampInfos);
+    
+    //$("#Container").append('');
+    
     $("#welcome").animate({
         opacity: 0,
     }, 1000, function () {
