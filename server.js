beeba7ff904f26a8649d8c4c311938f04725215e
FrancoisPala <francois.pala@epitech.eu>
2016-07-24 23:53:16 +0200

    Cleanup
    Less variables, clearer code

--- server.js
+++ server.js
@@ -9,7 +9,7 @@
 ** This is the server. For it to work, follow these instructions:
 ** See the example.json file in the project folder? Inside of it are instructions on how to make our program work.
 ** Those are needed because you need an API-Key to use the program. We can't let you see our API-Keys so we need you to use your own.
-** Just put it in th example.json file following the instructions, and then make sure you reference the path to this file in the "pathToYourApiKey" variable lower in the file.
+** Just put it in th example.json file following the instructions, and then make sure you reference the path to this file in the "ApiKey" variable lower in the file.
 */
 
 var port = 8083;
@@ -51,43 +51,33 @@ function main () {
     console.log("Server started listening on port: " + port);
     io.on('connection', function (socket) {
         let called = false; //this prevents users from spamming the "submit" button on the client and have us execute the same request too many times
-        let region = ""; //the summoner's region
-        let summonerName = ""; //the summoner's name
-
         socket.on("client info", function (data) {
             if (called == false){
               called = true;
               let tmp = data.split(",");
-              summonerName = tmp[0];
-              region = tmp[1].toLowerCase();
-
-              var a = sendInfo();
-
+              let summonerName = tmp[0];
+              let region = tmp[1].toLowerCase();
+              sendInfo();
               function sendInfo() {
                 Promise.all([
                     getGameInfo(region),
                     getPlayerInfo(region, summonerName)
                   ])
                   .then(values => {
-                    var send = mergeInfo(values[0], values[1]);
-                    socket.emit("info sent", send);
+                    socket.emit("info sent", mergeInfo(values[0], values[1]));
                     called = false;
                   });
-
                 function mergeInfo(versionChampions, playerInfo){
                   let toSend = {};
-
                   let championsMap = versionChampions[1];
                   let champMasteries = playerInfo[1];
                   toSend.infos = playerInfo[0];
-
                   for (let i = 0; i < champMasteries.length; ++i) {
                     let champId = champMasteries[i].championId;
                     let urlSquare = "http://ddragon.leagueoflegends.com/cdn/" + versionChampions[0] + "/img/champion/" + championsMap[champId][0] + ".png";
                     champMasteries[i].name = championsMap[champId][1];
                     champMasteries[i].riotName = championsMap[champId][0];
                     champMasteries[i].urlImage = urlSquare;
-
                     champId = championsMap[champId];
                   }
                   toSend.champMasteries = champMasteries;
@@ -100,8 +90,8 @@ function main () {
 }
 
 function getGameInfo(region) {
-  var a = getVersion(region);
-  var b = a.then(function (version) {
+  var promiseGetVersion = getVersion(region);
+  var promiseGetJSON = promiseGetVersion.then(function (version) {
     let req = "http://ddragon.leagueoflegends.com/cdn/" + version + "/data/en_US/champion.json " ;
     return rp({uri: req, json: true})
       .then( function(data) {
@@ -110,37 +100,33 @@ function getGameInfo(region) {
         for (let champ in str) {
           championsMap[str[champ].key] = [str[champ].id, str[champ].name] ;
         }
-        return Promise.resolve(championsMap);
+        return championsMap;
       })
       .catch (function (e) {
+        console.error("ChampionJSON Request Fail");
         return Promise.reject(new Error("ChampionJSON Request Fail"));
-      })
-      .catch(function(err) {
-        console.error(err);
       })
     ;
   });
-  return Promise.all([a, b]);
+  return Promise.all([promiseGetVersion, promiseGetJSON]);
 }
 
 function getVersion (region) {
     let req = "https://global.api.pvp.net/api/lol/static-data/" + region + "/v1.2/versions?api_key=" + ApiKey;
     return rp({uri: req, json: true})
       .then(function (data) {
-        return Promise.resolve(data[0]);
+        return data[0];
       })
       .catch(function(e) {
+        console.error("GetVersion Request Fail");
         return Promise.reject(new Error("GetVersion Request Fail"));
-      })
-      .catch(function(err) {
-        console.error(err);
       })
     ;
 }
 
 function getPlayerInfo(region, summonerName) {
-  let a = getSummonerId(region, summonerName);
-  let b = a.then(function(infos) {
+  let promiseGetSummId = getSummonerId(region, summonerName);
+  let promiseGetSummMastery = promiseGetSummId.then(function(infos) {
     let req = "https://" + region + ".api.pvp.net/championmastery/location/" + PLATFORMS[region] + "/player/" + infos.id + "/champions?api_key=" + ApiKey;
     return rp({uri: req, json: true})
       .then( function(str) {
@@ -149,24 +135,21 @@ function getPlayerInfo(region, summonerName) {
           delete str[i].lastPlayTime;
           delete str[i].championPointsSinceLastLevel;
         }
-        return Promise.resolve(str);
+        return str;
       })
       .catch( function(e) {
+        console.error("ChampionMastery Request Failed");
         return Promise.reject(new Error("ChampionMastery Request Failed"));
-      })
-      .catch(function(err) {
-        console.error(err);
       })
     ;
   });
-  return Promise.all([a, b]);
+  return Promise.all([promiseGetSummId, promiseGetSummMastery]);
 }
 
 function getSummonerId(region, summonerName) {
     let req = "https://" + region + ".api.pvp.net/api/lol/" + region + "/v1.4/summoner/by-name/" + summonerName + "?api_key=" + ApiKey;
     let sN = summonerName.replace(/\s+/g, '');
     sN = sN.toLowerCase();
-
     return rp({uri: req, json: true})
       .then(function (str) {
         let id = {
@@ -176,13 +159,11 @@ function getSummonerId(region, summonerName) {
           icon: "http://ddragon.leagueoflegends.com/cdn/6.9.1/img/profileicon/" + str[sN].profileIconId + ".png",
           level: str[sN].summonerLevel
         };
-        return Promise.resolve(id);
+        return id;
       })
       .catch(function(e) {
+        console.error("SummonerId Request Failed");
         return Promise.reject(new Error("SummonerId Request Failed"));
-      })
-      .catch(function(err) {
-        console.error(err);
       })
     ;
 }
